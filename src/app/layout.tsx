f31439a23b425888990a99bcab6c2a11f3fe56e4
Reffan Liefde <mail@reffanliefde.nl>
2025-02-01 12:37:24 +0700

    Added HeroUI.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next'
 
+import { Providers } from './providers'
+
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -15,8 +17,10 @@ type RootLayoutProps = Readonly<{
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en">
-      <body className={`antialiased`}>{children}</body>
+    <html lang="en" className="dark">
+      <body className={`antialiased`}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   )
 }
